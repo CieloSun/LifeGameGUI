ed4903a3ecde3c9c9bc5d9606d6e090e5ef5b71f
Cielo Sun <632898354@qq.com>
2016-04-10 16:31:10 +0800

    view the code and change the random method in mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -501,16 +501,16 @@ void MainWindow::fineFunction(){
             else if(Mainmap->cget(i, j).getState()==cell::DEAD) Mainmap->cget(i, j).init();
             else if(Mainmap->cget(i, j).getState()==cell::EMPTY)
             {
-                time_t seed = time(0) % 10000;
                 std::uniform_int_distribution<int> FineDistribution(0,9);
-                std::default_random_engine engine(seed);
+                std::default_random_engine engine;
+                engine.seed(std::chrono::system_clock::now().time_since_epoch().count());
                 int burnRandom=FineDistribution(engine);
                 std::cerr<<burnRandom<<std::endl;
                 if(burnRandom>=0&&burnRandom<=5)
                 {
                     Mainmap->cget(i, j).init(cell::LIVE,cell::PRODUCER);
                 }
-                else if(burnRandom>5&&burnRandom<=7)
+                else if(burnRandom>5&&burnRandom<=8)
                 {
                     Mainmap->cget(i, j).init(cell::LIVE,cell::CONSUMER);
                 }
